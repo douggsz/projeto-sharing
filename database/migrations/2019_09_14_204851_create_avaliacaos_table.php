d9890213859cfdfbc1af8a9f4e641165d93e6e73
Douglas Barbosa <douglasbarbosa030@gmail.com>
2021-03-24 11:29:10 -0300

    Update 2019_09_14_204851_create_avaliacaos_table.php

--- database/migrations/2019_09_14_204851_create_avaliacaos_table.php
+++ database/migrations/2019_09_14_204851_create_avaliacaos_table.php
@@ -4,15 +4,13 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAvaliacaosTable extends Migration
+class CreateVisualizacaoTable extends Migration
 {
     public function up()
     {
-        Schema::create('avaliacaos', function (Blueprint $table) {
+        Schema::create('visualizacaos', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->float('nota');
-            $table->boolean('like')->default(false);
-            $table->boolean('dislike')->default(false);
+            $table->foreignId('usuario_id')->constrained()->onDelete('cascade');
             $table->foreignId('conteudo_id')->constrained()->onDelete('cascade');
             $table->softDeletes();
             $table->timestamps();
